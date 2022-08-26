e511145d48dc0548baef6511b8a6aa11bcebea20
omar-shamim <omarfaruque94bd@gmail.com>
2022-08-27 00:32:01 +0600

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ReactGA from 'react-ga';
 import Routing from "./Routing";
 import { themes } from "./utils/theme";
+import { appSettings } from "./utils/appSettings";
 
 function App() {
+
+  useEffect(() => {
+    if (appSettings.googleTrackingID) {
+      ReactGA.initialize(appSettings.googleTrackingID,);
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+  }, []);
 
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
 
